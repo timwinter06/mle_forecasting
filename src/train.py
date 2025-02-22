8991a36c1545130c528c79dbb1cce25d8bc89136
Tim Winter <tim.winter@datasciencelab.nl>
2025-02-22 14:13:53 +0100

    clean up

--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+"""Module for training, evaluating and saving a random forest model."""
+
 import math
 import pickle
 from typing import Dict
@@ -7,7 +9,8 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-from preprocess import add_lagged_feature_to_df, create_train_test_split, preprocess, split_x_y
+from preprocess import Preprocesser
+from settings import DATA_PATH, MODEL_PATH
 
 
 class RFModelTrainer:
@@ -80,21 +83,19 @@ class RFModelTrainer:
 
 
 if __name__ == "__main__":
-    FILE_PATH = "./data/raw/dataset.csv"
-    df = pd.read_csv(FILE_PATH, sep=";", header=0)
-    df_cleaned = preprocess(df)
-    train_df, test_df = create_train_test_split(df_cleaned)
-    NUMBER_OF_LAGS = [7, 14, 21]  # 1 week ago, 2 weeks ago, 3 weeks ago
-    FEATURE_TO_LAG = "UnitSales"
-    train_df_lag = add_lagged_feature_to_df(train_df, NUMBER_OF_LAGS, FEATURE_TO_LAG)
-    test_df_lag = add_lagged_feature_to_df(test_df, NUMBER_OF_LAGS, FEATURE_TO_LAG)
-    train_x, train_y = split_x_y(train_df_lag)
-    test_x, test_y = split_x_y(test_df_lag)
+    # Load train/test data
+    preprocesser = Preprocesser(file_path=DATA_PATH)
+    train_x, train_y, test_x, test_y = preprocesser()
 
-    model = RFModelTrainer(
+    # Train model
+    model_trainer = RFModelTrainer(
         n_estimators=100, max_features=round(len(train_x.columns) / 3), max_depth=len(train_x.columns)
     )
+    model_trainer.train(train_x, train_y)
 
-    model.train(train_x, train_y)
-    metrics = model.evaluate(test_x, test_y)
+    # Evaluate model
+    metrics = model_trainer.evaluate(test_x, test_y)
     print(metrics)
+
+    # Save model
+    model_trainer.save(MODEL_PATH)
