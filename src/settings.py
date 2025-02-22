815f04e97a3002b866eeea540be5016d0947b8c5
Tim Winter <tim.winter@datasciencelab.nl>
2025-02-22 14:55:10 +0100

    add columns to settings

--- src/settings.py
+++ src/settings.py
@@ -7,3 +7,19 @@ MODEL_PATH = "./models/forecasting_model.pkl"
 # Hyperparameters
 NUMBER_OF_LAGS = [7, 14, 21]
 FEATURE_TO_LAG = "UnitSales"
+
+# Data columns
+COLUMNS = [
+    "StoreCount",
+    "ShelfCapacity",
+    "PromoShelfCapacity",
+    "IsPromo",
+    "ItemNumber",
+    "CategoryCode",
+    "GroupCode",
+    "month",
+    "weekday",
+    "UnitSales_-7",
+    "UnitSales_-14",
+    "UnitSales_-21",
+]
